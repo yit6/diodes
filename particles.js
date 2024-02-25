0c71c8c4e51538bb366c066af04c14f473283f3e
yit6 <thatsjustwyatt@gmail.com>
2024-02-25 09:41:06 -0500

    sutff

--- particles.js
+++ particles.js
@@ -9,7 +9,11 @@ const canvas_offset = canvas.getBoundingClientRect();
 
 let particles = [];
 
-for (let i = 0; i < 170; ++i) {
+const num = 250;
+const forceUser = 20;
+const forcePart = 0;
+
+for (let i = 0; i < num; ++i) {
 	particles.push({
 		x:canvas.width*Math.random(),
 		y:canvas.height*Math.random(),
@@ -23,10 +27,29 @@ addEventListener("mousemove", (e) => {
 	mouseY = e.clientY - canvas_offset.top;
 });
 
+addEventListener("touchmove", (e) => {
+	mouseX = e.clientX - canvas_offset.left;
+	mouseY = e.clientY - canvas_offset.top;
+});
+
+apply = (p, x, y, force) => {
+		mag = force/((p.x-x)*(p.x-x)+(p.y-y)*(p.y-y));
+
+		p.xSpd += (p.x-x)*mag;
+		p.ySpd += (p.y-y)*mag;
+}
+
 draw = () => {
 	ctx.clearRect(0,0,canvas.width,canvas.height);
 
 	ctx.fillRect(mouseX, mouseY, 10, 10);
+
+	for (let i = 0; i < num; ++i) {
+		for (let j = i+1; j < num; ++j) {
+			apply(particles[i], particles[j].x, particles[j].y, forcePart);
+			apply(particles[j], particles[i].x, particles[i].y, forcePart);
+		}
+	}
 
 	particles.forEach((p) => {
 		p.x += p.xSpd;
@@ -35,13 +58,12 @@ draw = () => {
 		p.x = (p.x+canvas.width) % canvas.width;
 		p.y = (p.y+canvas.height) % canvas.height;
 
-		p.xSpd *= 0.99;
-		p.ySpd *= 0.99;
+		p.xSpd *= 0.94;
+		p.ySpd *= 0.94;
 
-		mag = 20/((p.x-mouseX)*(p.x-mouseX)+(p.y-mouseY)*(p.y-mouseY));
-
-		p.xSpd += (p.x-mouseX)*mag;
-		p.ySpd += (p.y-mouseY)*mag;
+		if (0 <= mouseX && mouseX <= canvas.width && 0 <= mouseY && mouseY <= canvas.height) {
+			apply(p, mouseX, mouseY, forceUser);
+		}
 
 		ctx.beginPath()
 		ctx.arc(p.x, p.y, 2, 0, 2*3.15);
