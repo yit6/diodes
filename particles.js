8fec7db42c356ee942cadafd3c9846df9bba188f
yit6 <thatsjustwyatt@gmail.com>
2024-02-25 09:06:21 -0500

    particles

--- particles.js
+++ particles.js
@@ -1,5 +1,49 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 200, 200);
+ctx.fillStyle = "blue";
+
+let mouseX = 0; let mouseY = 0;
+
+const canvas_offset = canvas.getBoundingClientRect();
+
+let particles = [];
+
+for (let i = 0; i < 100; ++i) {
+	particles.push({
+		x:canvas.width*Math.random(),
+		y:canvas.height*Math.random(),
+		xSpd:Math.random()*2-1,
+		ySpd:Math.random()*2-1,
+	});
+}
+
+addEventListener("mousemove", (e) => {
+	mouseX = e.clientX - canvas_offset.left;
+	mouseY = e.clientY - canvas_offset.top;
+});
+
+draw = () => {
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+
+	ctx.fillRect(mouseX, mouseY, 10, 10);
+
+	particles.forEach((p) => {
+		p.x += p.xSpd;
+		p.y += p.ySpd;
+
+		p.x = (p.x+canvas.width) % canvas.width;
+		p.y = (p.y+canvas.height) % canvas.height;
+
+		p.xSpd *= 0.99;
+		p.ySpd *= 0.99;
+
+		mag = 100/((p.x-mouseX)*(p.x-mouseX)+(p.y-mouseY)*(p.y-mouseY));
+
+		ctx.beginPath()
+		ctx.arc(p.x, p.y, 2, 0, 2*3.15);
+		ctx.fill();
+	});
+}
+
+setInterval(draw, 40);
